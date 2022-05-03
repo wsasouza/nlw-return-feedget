cca22434fe97456c101b85671d7af1ff97f777dd
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-03 17:19:19 -0300

    Feat: Create Screenshot Button

--- frontend/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ frontend/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { ArrowLeft } from 'phosphor-react';
+
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
+import { ScreenshotButton } from '../ScreenshotButton';
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -11,6 +14,8 @@ export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -41,6 +46,20 @@ export function FeedbackContentStep({
           placeholder="Conte com detalhes o que está acontecendo..."
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-1 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
         />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
       </form>
     </>
   );
