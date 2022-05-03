7e75a0fd98852c8ac896b29f7cdf1f2d7b69eed8
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-03 12:06:52 -0300

    Feat: Create Feedback Form

---
+++ frontend/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,7 @@
+export function FeedbackContentStep() {
+  return (
+    <div>
+      <h1>FeedbackContentStep</h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,7 @@
+export function FeedbackSuccessStep() {
+  return (
+    <div>
+      <h1>FeedbackSuccessStep</h1>
+    </div>
+  );
+}
