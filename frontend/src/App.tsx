fba12bce9395113ec1c1aa11de2ffb04fc6106ab
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-02 23:20:04 -0300

    Chore: Configure project vite and tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-orange-500">Hello NLW Return</h1>
+      <h2>Testando Hot reloading vitejs</h2>
+      <h3>será que vai funfar?</h3>
+    </div>
+  );
+}
+
+export default App;
