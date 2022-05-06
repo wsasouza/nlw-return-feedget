33bf1756c6bd23d33057c997635a627c1132e4c1
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-05 22:31:19 -0300

    Feat: Create Widget

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,30 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StyleSheet, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+
+import Widget from './src/components/Widget';
+import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
@@ -13,8 +32,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colors.background,
   },
 });
---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots, X } from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { styles } from './styles';
+import { theme } from '../../theme';
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots
+          weight="bold"
+          size={24}
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <X />
+      </BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
