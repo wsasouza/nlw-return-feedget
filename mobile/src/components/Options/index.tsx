eda6663e4c211c06482c9b01e54d4e8ba15d8eda
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-05-05 23:35:43 -0300

    Feat: Create Options Component

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Copyright } from '../Copyright';
 
+import { feedbackTypes } from '../../utils/feedbackTypes';
 import { styles } from './styles';
+import { Option } from '../Option';
 
 export function Options() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Deixe seu feedback</Text>
+
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option key={key} title={value.title} image={value.image} />
+        ))}
+      </View>
+
       <Copyright />
     </View>
   );
